24dde664ddc221771a181d1fcbce6061121f57bb
md sium <55883376+mdsium@users.noreply.github.com>
2023-09-16 15:29:09 +0600

    published
    Restaurant website landing page PSD to HTML

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+    $(".hamburger-nav").on("click", function(){
+  
+      // $(".menu").fadeToggle("slow").toggleClass("menu-hide");
+      $(".menu").animate({
+        height: 'toggle'
+      });
+  
+    });
+  
+  });
